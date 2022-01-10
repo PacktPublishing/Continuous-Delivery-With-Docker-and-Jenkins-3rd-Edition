e0449182b22875839e0f44a0e7a0599b5b3e8361
Rafal Leszko <rafal@livepeer.org>
2022-01-10 17:34:06 +0100

    Update Chapter06 sample6

--- Chapter06/sample6/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter06/sample6/src/main/java/com/leszko/calculator/Calculator.java
@@ -1,9 +1,18 @@
 package com.leszko.calculator; 
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service; 
  
 @Service 
 public class Calculator { 
+     @Cacheable("sum")
      int sum(int a, int b) { 
+          try { 
+               Thread.sleep(3000); 
+          } 
+          catch (InterruptedException e) { 
+               e.printStackTrace(); 
+          }
           return a + b; 
      } 
 } 
